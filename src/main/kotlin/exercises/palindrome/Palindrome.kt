95456c6fb07be6545edff00f5c5a97ed878f9418
Edgardo Barría <edgardo.barriam@gmail.com>
2024-07-15 21:05:45 -0400

    add directions and examples comments

--- src/main/kotlin/exercises/palindrome/Palindrome.kt
+++ src/main/kotlin/exercises/palindrome/Palindrome.kt
@@ -2,6 +2,16 @@ package exercises.palindrome
 
 class Palindrome {
 
+  /*
+  --- Directions
+  Given a string, return true if the string is a palindrome
+  or false if it is not.  Palindromes are strings that
+  form the same word if it is reversed. *Do* include spaces
+  and punctuation in determining if the string is a palindrome.
+  --- Examples:
+  palindrome("abba") === true
+  palindrome("abcdefg") === false
+  */
   fun isPalindrome(str: String): Boolean {
     return str == str.reversed()
   }
--- src/main/kotlin/exercises/reversestring/ReverseString.kt
+++ src/main/kotlin/exercises/reversestring/ReverseString.kt
@@ -2,6 +2,15 @@ package exercises.reversestring
 
 class ReverseString {
 
+  /*
+  --- Directions
+  Given a string, return a new string with the reversed
+  order of characters
+  --- Examples
+  reverse('apple') === 'leppa'
+  reverse('hello') === 'olleh'
+  reverse('Greetings!') === '!sgniteerG'
+  */
   fun reverse(string: String): String {
     var reversed = ""
     string.forEach { reversed = it + reversed }
