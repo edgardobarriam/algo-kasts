f99d306721a82b06290efaa4780ed36c7c02b6fe
Edgardo Barría <edgardo.barriam@gmail.com>
2024-07-18 19:28:49 -0400

    add fizzbuzz instructions

--- src/main/kotlin/exercises/fizzbuzz/FizzBuzz.kt
+++ src/main/kotlin/exercises/fizzbuzz/FizzBuzz.kt
@@ -2,11 +2,26 @@ package exercises.fizzbuzz
 
 class FizzBuzz {
 
-  fun fizzBuzz(n: Int) : List<String> {
+  /*
+  --- Directions
+  Write a program that console logs the numbers
+  from 1 to n. But for multiples of three print
+  “fizz” instead of the number and for the multiples
+  of five print “buzz”. For numbers which are multiples
+  of both three and five print “fizzbuzz”.
+  --- Example
+  fizzBuzz(5);
+  1
+  2
+  fizz
+  4
+  buzz
+  */
+  fun fizzBuzz(n: Int): List<String> {
     val values = mutableListOf<String>()
     for (i in 1..n) {
-      var msg = if (i%3 == 0) "fizz" else ""
-      msg += if (i%5 == 0) "buzz" else ""
+      var msg = if (i % 3 == 0) "fizz" else ""
+      msg += if (i % 5 == 0) "buzz" else ""
       values += msg.ifEmpty { i.toString() }
     }
     return values
